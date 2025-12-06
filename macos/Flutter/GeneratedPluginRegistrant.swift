154d4786946eec06694accea5b8b6032b2af37f5
alidarovolj <alidarovolzhas@gmail.com>
2025-12-06 20:35:20 +0500

    feat: обновлены Firebase зависимости до версии 11.15.0
    - firebase_core: 2.32.0 -> 3.15.2
    - firebase_messaging: 14.9.4 -> 15.2.10
    - firebase_analytics: 10.10.10 -> 11.6.0
    - firebase_remote_config: 4.4.10 -> 5.5.0
    
    - Упрощен Podfile по образцу рабочей версии
    - Убраны явные зависимости Firebase pods
    - Добавлен source 'https://cdn.cocoapods.org/'
    - Установлены Firebase SDK 11.15.0

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_analytics
 import firebase_core
 import firebase_messaging
+import firebase_remote_config
 import flutter_local_notifications
 import path_provider_foundation
 import shared_preferences_foundation
@@ -14,8 +16,10 @@ import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FirebaseRemoteConfigPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
